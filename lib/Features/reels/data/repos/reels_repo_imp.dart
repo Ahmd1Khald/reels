d6ba200cdc683157c450133d5497fe76955c4024
Ahmd1Khald <ahmed.khaled.sayed2000@gmail.com>
2024-08-02 20:05:28 +0300

    handle remove api reels

--- lib/Features/reels/data/repos/reels_repo_imp.dart
+++ lib/Features/reels/data/repos/reels_repo_imp.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
+import '../../../../Core/utiles/api_const.dart';
+import '../../../../Core/utiles/api_service.dart';
 import '../../../../core/errors/failures.dart';
 import '../models/reels_model.dart';
 import '../models/watched_model.dart';
@@ -10,52 +12,52 @@ class ReelsRepoImpl implements ReelsRepo {
   @override
   Future<Either<Failure, List<ReelsModel>>> fetchReels() async {
     try {
-      // Response response = await DioHelper.getData(
-      //   url: "${ApiConst.baseUrl}reels",
-      // ); // Replace with your API endpoint
+      Response response = await DioHelper.getData(
+        url: "${ApiConst.baseUrl}reels",
+      ); // Replace with your API endpoint
 
-      var response = {
-        "reels": [
-          {
-            "_id": "66ad52498692ff76fbe9a2db",
-            "url": "https://youtube.com/shorts/SpY6HQYkHuc?si=54d3iXpC-3vMoPJX",
-            "createdAt": "2024-08-02T21:40:25.771Z",
-            "expiresAt": "2024-08-03T21:40:25.772Z",
-            "__v": 0
-          },
-          {
-            "_id": "66ad52498692ff76fbe9a2db",
-            "url": "https://youtube.com/shorts/SpY6HQYkHuc?si=54d3iXpC-3vMoPJX",
-            "createdAt": "2024-08-02T21:40:25.771Z",
-            "expiresAt": "2024-08-03T21:40:25.772Z",
-            "__v": 0
-          },
-          {
-            "_id": "66ad52498692ff76fbe9a2db",
-            "url": "https://youtube.com/shorts/SpY6HQYkHuc?si=54d3iXpC-3vMoPJX",
-            "createdAt": "2024-08-02T21:40:25.771Z",
-            "expiresAt": "2024-08-03T21:40:25.772Z",
-            "__v": 0
-          },
-          {
-            "_id": "66ad52498692ff76fbe9a2db",
-            "url": "https://youtube.com/shorts/SpY6HQYkHuc?si=54d3iXpC-3vMoPJX",
-            "createdAt": "2024-08-02T21:40:25.771Z",
-            "expiresAt": "2024-08-03T21:40:25.772Z",
-            "__v": 0
-          },
-        ]
-      };
+      //var response = {
+      //   "reels": [
+      //     {
+      //       "_id": "66ad52498692ff76fbe9a2db",
+      //       "url": "https://youtube.com/shorts/SpY6HQYkHuc?si=54d3iXpC-3vMoPJX",
+      //       "createdAt": "2024-08-02T21:40:25.771Z",
+      //       "expiresAt": "2024-08-03T21:40:25.772Z",
+      //       "__v": 0
+      //     },
+      //     {
+      //       "_id": "66ad52498692ff76fbe9a2db",
+      //       "url": "https://youtube.com/shorts/SpY6HQYkHuc?si=54d3iXpC-3vMoPJX",
+      //       "createdAt": "2024-08-02T21:40:25.771Z",
+      //       "expiresAt": "2024-08-03T21:40:25.772Z",
+      //       "__v": 0
+      //     },
+      //     {
+      //       "_id": "66ad52498692ff76fbe9a2db",
+      //       "url": "https://youtube.com/shorts/SpY6HQYkHuc?si=54d3iXpC-3vMoPJX",
+      //       "createdAt": "2024-08-02T21:40:25.771Z",
+      //       "expiresAt": "2024-08-03T21:40:25.772Z",
+      //       "__v": 0
+      //     },
+      //     {
+      //       "_id": "66ad52498692ff76fbe9a2db",
+      //       "url": "https://youtube.com/shorts/SpY6HQYkHuc?si=54d3iXpC-3vMoPJX",
+      //       "createdAt": "2024-08-02T21:40:25.771Z",
+      //       "expiresAt": "2024-08-03T21:40:25.772Z",
+      //       "__v": 0
+      //     },
+      //   ]
+      // };
 
       print("+++++++++++++response++++++++++++++");
-      //print(response.data["reels"]);
-      print(response["reels"]?[1]);
+      print(response.data["reels"]);
+      //print(response["reels"]?[1]);
 
-      //List<dynamic> reelsJson = response.data['reels'];
-      List<dynamic>? reelsJson = response['reels'];
+      List<dynamic> reelsJson = response.data['reels'];
+      //List<dynamic>? reelsJson = response['reels'];
 
       List<ReelsModel> reelsList =
-          reelsJson!.map((json) => ReelsModel.fromJson(json)).toList();
+          reelsJson.map((json) => ReelsModel.fromJson(json)).toList();
 
       print("+++++++++++++reelsList++++++++++++++");
       print(reelsList);
